d418c1b2126d833bfa5bbde8bf19386c2ae8e50d
Duartec <Duartec@IT-WKS017.MidNet.CityOfMiddletown.com>
2022-08-16 11:28:12 -0400

    Initial Commit

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,7 +6,21 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Db',
+    'Laminas\Navigation',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\I18n',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
     'Laminas\Router',
     'Laminas\Validator',
+    'Laminas\ZendFrameworkBridge',
+    'Laminas\DeveloperTools',
     'Application',
 ];
