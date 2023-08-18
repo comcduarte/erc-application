0c5d4f6db0fbad711ac4ecf7a596dfcf6eb5d962
Duartec <Duartec@IT-WKS017.MidNet.CityOfMiddletown.com>
2023-08-18 16:24:36 -0400

    Moved config to .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,5 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-python:
-    install:
-        requirements: docs/requirements.txt
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
